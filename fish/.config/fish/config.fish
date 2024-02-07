703c14b0dc9ecddc49fb2391dc7fbd0dab26445b
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2024-02-07 20:34:02 +0400

    feat(fish): add zoxide init

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -24,3 +24,7 @@ fish_add_path $BUN_INSTALL/bin $PATH
 # set -x PATH $BUN_INSTALL/bin $PATH
 
 fish_add_path ~/.local/share/gem/ruby/3.0.0/gems/tmuxinator-3.0.5/bin
+
+if type -q zoxide
+  zoxide init fish | source
+end
