b3269f72d5cc8f8a29cfca4e66114b8d13c25e06
Dmitry Krasiev <dkrasiev@datafire.ru>
2024-04-23 17:28:13 +0400

    feat(nvim): return community plugins from previous config

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,6 +5,36 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+
+  { import = "astrocommunity.completion.codeium-vim" },
+  { import = "astrocommunity.completion.cmp-cmdline" },
+
+  { import = "astrocommunity.git.blame-nvim" },
+  { import = "astrocommunity.git.git-blame-nvim" },
+
+  { import = "astrocommunity.motion.nvim-surround" },
+
+  -- JS web dev
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.angular" },
+  -- misc for web dev
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.prisma" },
+
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.kotlin" },
+  { import = "astrocommunity.pack.java" },
+
+  { import = "astrocommunity/file-explorer/oil-nvim" },
+  { import = "astrocommunity/note-taking/obsidian-nvim" },
+  { import = "astrocommunity/fuzzy-finder/telescope-zoxide" },
+  { import = "astrocommunity/editing-support/rainbow-delimiters-nvim" },
+  { import = "astrocommunity/project/nvim-spectre" },
 }
