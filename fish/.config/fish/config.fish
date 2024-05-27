7f0d5497447b612a9f6b51e18a09acb335310186
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2024-05-27 22:40:55 +0400

    feat(fish): update config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,16 +7,17 @@ end
 fish_config theme choose "Rosé Pine"
 
 # environment variables
-set -x EDITOR nvim 
-set -x VISUAL nvim 
+set -x EDITOR nvim
+set -x VISUAL nvim
 
 alias gl='git log --oneline --graph'
+alias v='nvim'
 
 # remove fish greeting message
 set fish_greeting
 
 # remove nvm message
-nvm use lts > /dev/null
+nvm use lts >/dev/null
 
 # bun
 set -x BUN_INSTALL "$HOME/.bun"
@@ -26,5 +27,5 @@ fish_add_path $BUN_INSTALL/bin $PATH
 fish_add_path ~/.local/share/gem/ruby/3.0.0/gems/tmuxinator-3.0.5/bin
 
 if type -q zoxide
-  zoxide init fish | source
+    zoxide init fish | source
 end
