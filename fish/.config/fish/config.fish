4a9da419178e16476084ce93a4c6fdc190070379
Dmitry Krasiev <dkrasiev@datafire.ru>
2024-05-28 11:00:49 +0400

    feat(fish): update config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -17,14 +17,14 @@ alias v='nvim'
 set fish_greeting
 
 # remove nvm message
-nvm use lts >/dev/null
+# nvm use lts >/dev/null
 
 # bun
-set -x BUN_INSTALL "$HOME/.bun"
-fish_add_path $BUN_INSTALL/bin $PATH
+# set -x BUN_INSTALL "$HOME/.bun"
+# fish_add_path $BUN_INSTALL/bin $PATH
 # set -x PATH $BUN_INSTALL/bin $PATH
 
-fish_add_path ~/.local/share/gem/ruby/3.0.0/gems/tmuxinator-3.0.5/bin
+# fish_add_path ~/.local/share/gem/ruby/3.0.0/gems/tmuxinator-3.0.5/bin
 
 if type -q zoxide
     zoxide init fish | source
