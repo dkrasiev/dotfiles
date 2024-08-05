689c3d533052bac54f69733878fcd563bd4e8388
Dmitry Krasiev <dkrasiev@datafire.ru>
2024-08-05 12:33:53 +0400

    feat(nvim-lazyvim): disable firenvim by default

--- nvim-lazyvim/.config/nvim/lua/plugins/firenvim.lua
+++ nvim-lazyvim/.config/nvim/lua/plugins/firenvim.lua
@@ -4,7 +4,19 @@ return {
     -- Lazy load firenvim
     -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
     lazy = not vim.g.started_by_firenvim,
+    module = false,
     build = function()
-        vim.fn["firenvim#install"](0)
-    end
+        vim.fn['firenvim#install'](0)
+    end,
+
+    config = function()
+      vim.g.firenvim_config = {
+        globalSettings = {},
+        localSettings = {
+          [".*"] = {
+            takeover = "never",
+          },
+        },
+      }
+    end,
 }
