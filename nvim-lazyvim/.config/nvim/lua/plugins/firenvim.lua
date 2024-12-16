18bedfbb1b8d97a95698afba2cf36ad9757413f8
Dmitry Krasiev <dkrasiev@datafire.ru>
2024-12-16 14:16:29 +0400

    feat(nvim-lazyvim): update firenvim plugin config

--- nvim-lazyvim/.config/nvim/lua/plugins/firenvim.lua
+++ nvim-lazyvim/.config/nvim/lua/plugins/firenvim.lua
@@ -1,22 +1,10 @@
 return {
-    'glacambre/firenvim',
-
+    "glacambre/firenvim",
     -- Lazy load firenvim
     -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
     lazy = not vim.g.started_by_firenvim,
     module = false,
     build = function()
-        vim.fn['firenvim#install'](0)
-    end,
-
-    config = function()
-      vim.g.firenvim_config = {
-        globalSettings = {},
-        localSettings = {
-          [".*"] = {
-            takeover = "never",
-          },
-        },
-      }
+        vim.fn["firenvim#install"](0)
     end,
 }
