5695dda207a8333fb6f69c8791d2fa20f16f2e1a
Dmitry Krasiev <dkrasiev@datafire.ru>
2024-12-19 12:11:00 +0400

    feat(nvim-lazyvim): add colorscheme plugin

---
+++ nvim-lazyvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = 'gruvbox',
+      -- colorscheme = 'kanagawa',
+      colorscheme = 'rose-pine',
+    },
+  },
+
+  -- { "ellisonleao/gruvbox.nvim" },
+
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  --   opts = {
+  --     theme = "wave",
+  --   },
+  -- },
+
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      styles = {
+        transparency = true,
+      },
+    },
+  }
+}
